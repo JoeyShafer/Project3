a66b4179ed58d2f2c4ce1c0ca766d7d2151546b5
Joey Shafer <Joseph.m.shafer-1@ou.edu>
2019-10-22 21:41:46 -0500

    Inital commit

---
+++ DateTimeOne.java
@@ -0,0 +1,27 @@
+
+public class DateTimeOne extends MesoDateTimeOneAbstract
+{
+   public int getValueOfSecond() {
+	   
+   }
+   
+   public void dateTimeNow() {
+	   
+   }
+   
+   public void sleepForFiveSec() {
+	   
+   }
+   
+   public void dateTimeForOtherCity() {
+	   
+   }
+   
+   public void dateTimeDifferentZone() {
+	   
+   }
+   
+   public void timeZoneHashMap() {
+	   
+   }
+}
---
+++ DateTimeOneTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+class DateTimeOneTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getSecondTest() {
+		long time = System.currentTimeMillis();
+		int second = (int)time/1000;
+		
+	}
+
+}
